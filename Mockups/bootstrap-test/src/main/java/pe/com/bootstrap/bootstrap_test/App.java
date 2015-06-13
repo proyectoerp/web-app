10a99231c07df2ed11441ce931b56b8c06e0680a
brianpari <brianpari@gmail.com>
2015-06-13 14:43:39 -0500

    Agregando Mockups Folder
    se agrego primer mockup de pantalla gestión de materiales

---
+++ Mockups/bootstrap-test/src/main/java/pe/com/bootstrap/bootstrap_test/App.java
@@ -0,0 +1,13 @@
+package pe.com.bootstrap.bootstrap_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
