5fbfe9e7fed77cb875fda3c787b1d650e130627e
jose <joseruizp@gmail.com>
2015-12-02 22:01:02 -0500

    ver planificacion

--- ERPProject2/target/classes/static/js/app/app.js
+++ ERPProject2/target/classes/static/js/app/app.js
@@ -24,30 +24,101 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
         templateUrl: 'partials/agregarEditarMateriales',
         controller: 'materialsController'
     })
+    .state('gestionCentros', {
+        url:'/gestionCentros',
+        templateUrl: 'partials/gestionCentros',
+        controller: 'centersController'
+    })
+    .state('agregarCentros', {
+        url:'/agregarEditarCentros',
+        templateUrl: 'partials/agregarEditarCentros',
+        controller: 'centersController'
+    })
+    .state('editarCentros', {
+        url:'/editarCentros/:centroId',
+        templateUrl: 'partials/agregarEditarCentros',
+        controller: 'centersController'
+    })
 	.state('guiaEntrada', {
 		url:'/guiaEntrada',
 		controller: 'materialsController',
 		templateUrl: 'partials/guiaEntrada'
+	})
+	.state('agregarGuiaEntrada', {
+		url:'/agregarGuiaEntrada',
+		controller: 'materialsController',
+		templateUrl: 'partials/agregarEditarGuiaEntrada'
+	})
+	.state('editarGuiaEntrada', {
+		url:'/guiaEntrada',
+		controller: 'materialsController',
+		templateUrl: 'partials/agregarEditarGuiaEntrada'
 	})
 	.state('listaMateriales', {
 		url:'/listaMateriales',
 		controller: 'materialsController',
 		templateUrl: 'partials/listaMateriales'
 	})
+	.state('agregarListaMateriales', {
+		url:'/agregarListaMateriales',
+		controller: 'materialsController',
+		templateUrl: 'partials/agregarEditarListaMateriales'
+	})
+	.state('editarListaMateriales', {
+		url:'/editarListaMateriales',
+		controller: 'materialsController',
+		templateUrl: 'partials/agregarEditarListaMateriales'
+	})
 	.state('centroTrabajo', {
 		url:'/centroTrabajo',
-		controller: 'materialsController',
+		controller: 'workCentersController',
 		templateUrl: 'partials/centroTrabajo'
+	})
+	.state('agregarCentroTrabajo', {
+		url:'/agregarCentroTrabajo',
+		controller: 'workCentersController',
+		templateUrl: 'partials/agregarEditarCentroTrabajo'
+	})
+	.state('editarCentroTrabajo', {
+		url:'/editarCentroTrabajo',
+		controller: 'workCentersController',
+		templateUrl: 'partials/agregarEditarCentroTrabajo'
 	})
 	.state('rutaFabricacion', {
 		url:'/rutaFabricacion',
-		controller: 'materialsController',
+		controller: 'fabricationRoutesController',
 		templateUrl: 'partials/rutaFabricacion'
+	})
+	.state('agregarRutaFabricacion', {
+		url:'/agregarRutaFabricacion',
+		controller: 'fabricationRoutesController',
+		templateUrl: 'partials/agregarEditarRutaFabricacion'
+	})
+	.state('editarRutaFabricacion', {
+		url:'/editarRutaFabricacion',
+		controller: 'fabricationRoutesController',
+		templateUrl: 'partials/agregarEditarRutaFabricacion'
 	})
 	.state('planificadorNecesidades', {
 		url:'/planificadorNecesidades',
-		controller: 'materialsController',
+		controller: 'necesityPlannerController',
 		templateUrl: 'partials/planificadorNecesidades'
+	})
+	.state('agregarPlanificadorNecesidades', {
+		url:'/agregarPlanificadorNecesidades',
+		controller: 'necesityPlannerController',
+		templateUrl: 'partials/agregarEditarPlanificadorNecesidades'
+	})
+	.state('agregarEditarPlanificadorNecesidades', {
+		url:'/editarPlanificadorNecesidades',
+		controller: 'necesityPlannerController',
+		templateUrl: 'partials/agregarEditarPlanificadorNecesidades'
+	})
+	.state('gestionFamilia', {
+		url:'/gestionFamilia',
+		controller: 'familyController',
+		templateUrl: 'partials/gestionFamilia'
 	});
+	
 	
 }]);
